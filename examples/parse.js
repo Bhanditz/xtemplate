24c4cb2ea8e334a1cffd984af8f0ec32ff2ef8a2
yiminghe <yiminghe@gmail.com>
2017-07-12 13:43:20 +0800

    Updates

--- examples/parse.js
+++ examples/parse.js
@@ -1,76 +1,74 @@
-webpackJsonp([1],{
+webpackJsonp([2],{
 
-/***/ 0:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 343:
+/***/ (function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(331);
+module.exports = __webpack_require__(90);
 
 
-/***/ },
+/***/ }),
 
-/***/ 331:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 90:
+/***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	/* eslint no-console:0 */
-	
-	var React = __webpack_require__(2);
-	var ReactDOM = __webpack_require__(36);
-	var XTemplate = __webpack_require__(176);
-	
-	var Test = React.createClass({
-	  displayName: 'Test',
-	  parse: function parse() {
-	    var g = XTemplate.Compiler.parse(this.refs.tpl.value);
-	    console.log(g);
-	  },
-	  render: function render() {
-	    return React.createElement(
-	      'div',
-	      null,
-	      React.createElement('link', { rel: 'stylesheet', href: '//g.tbcdn.cn/kissy/k/1.4.2/css/dpl/base.css' }),
-	      React.createElement('link', { rel: 'stylesheet', href: '//g.tbcdn.cn/kissy/k/1.4.2/css/dpl/forms.css' }),
-	      React.createElement('link', { rel: 'stylesheet', href: '//g.tbcdn.cn/kissy/k/1.4.2/button/assets/dpl.css' }),
-	      React.createElement(
-	        'div',
-	        { className: 'container' },
-	        React.createElement(
-	          'div',
-	          { className: 'row' },
-	          React.createElement(
-	            'div',
-	            { className: 'span8' },
-	            React.createElement(
-	              'h2',
-	              null,
-	              '模板代码'
-	            ),
-	            React.createElement(
-	              'div',
-	              null,
-	              React.createElement('textarea', {
-	                style: { width: 350, height: 400 },
-	                ref: 'tpl',
-	                defaultValue: '\n                    {{3*4*5}}\n'
-	              })
-	            ),
-	            React.createElement('br', null),
-	            React.createElement(
-	              'button',
-	              { ref: 'parse', className: 'ks-button', onClick: this.parse },
-	              'parse'
-	            )
-	          )
-	        )
-	      )
-	    );
-	  }
-	});
-	
-	ReactDOM.render(React.createElement(Test, null), document.getElementById('__react-content'));
+/* eslint no-console:0 */
 
-/***/ }
+var React = __webpack_require__(54);
+var ReactDOM = __webpack_require__(37);
+var XTemplate = __webpack_require__(32);
 
+var Test = React.createClass({
+  displayName: 'Test',
+  parse: function parse() {
+    var g = XTemplate.Compiler.parse(this.refs.tpl.value);
+    console.log(g);
+  },
+  render: function render() {
+    return React.createElement(
+      'div',
+      null,
+      React.createElement('link', { rel: 'stylesheet', href: '//g.tbcdn.cn/kissy/k/1.4.2/css/dpl/base.css' }),
+      React.createElement('link', { rel: 'stylesheet', href: '//g.tbcdn.cn/kissy/k/1.4.2/css/dpl/forms.css' }),
+      React.createElement('link', { rel: 'stylesheet', href: '//g.tbcdn.cn/kissy/k/1.4.2/button/assets/dpl.css' }),
+      React.createElement(
+        'div',
+        { className: 'container' },
+        React.createElement(
+          'div',
+          { className: 'row' },
+          React.createElement(
+            'div',
+            { className: 'span8' },
+            React.createElement(
+              'h2',
+              null,
+              '\u6A21\u677F\u4EE3\u7801'
+            ),
+            React.createElement(
+              'div',
+              null,
+              React.createElement('textarea', {
+                style: { width: 350, height: 400 },
+                ref: 'tpl',
+                defaultValue: '\n                    {{3*4*5}}\n'
+              })
+            ),
+            React.createElement('br', null),
+            React.createElement(
+              'button',
+              { ref: 'parse', className: 'ks-button', onClick: this.parse },
+              'parse'
+            )
+          )
+        )
+      )
+    );
+  }
 });
+
+ReactDOM.render(React.createElement(Test, null), document.getElementById('__react-content'));
+
+/***/ })
+
+},[343]);
 //# sourceMappingURL=parse.js.map
--- examples/playground.js
+++ examples/playground.js
@@ -1,80 +1,78 @@
-webpackJsonp([2],{
+webpackJsonp([1],{
 
-/***/ 0:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 344:
+/***/ (function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(332);
+module.exports = __webpack_require__(91);
 
 
-/***/ },
+/***/ }),
 
-/***/ 332:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 91:
+/***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	/* eslint no-console:0 */
-	
-	var React = __webpack_require__(2);
-	var ReactDOM = __webpack_require__(36);
-	var XTemplate = __webpack_require__(176);
-	var textareaStyle = {
-	  width: 600,
-	  height: 200
-	};
-	var labelStyle = {
-	  display: 'inline-block',
-	  width: 100
-	};
-	
-	function start() {
-	  var data = {};
-	  var dataContent = document.getElementById('data').value.trim();
-	  if (dataContent) {
-	    data = JSON.parse(dataContent);
-	  }
-	  console.log(new XTemplate(document.getElementById('template').value).render(data));
-	}
-	
-	ReactDOM.render(React.createElement(
-	  'div',
-	  { style: { margin: 10 } },
-	  React.createElement(
-	    'div',
-	    { style: { marginBottom: 10 } },
-	    React.createElement(
-	      'span',
-	      { style: labelStyle },
-	      'template:'
-	    ),
-	    React.createElement('textarea', {
-	      style: textareaStyle,
-	      id: 'template',
-	      defaultValue: 'Hello {{world}}!'
-	    })
-	  ),
-	  React.createElement(
-	    'div',
-	    { style: { marginBottom: 10 } },
-	    React.createElement(
-	      'span',
-	      { style: labelStyle },
-	      'data(json):'
-	    ),
-	    React.createElement('textarea', {
-	      style: textareaStyle,
-	      id: 'data',
-	      defaultValue: '{ "world": "world"}'
-	    })
-	  ),
-	  React.createElement(
-	    'button',
-	    { onClick: start },
-	    'render to console'
-	  )
-	), document.getElementById('__react-content'));
+/* eslint no-console:0 */
 
-/***/ }
+var React = __webpack_require__(54);
+var ReactDOM = __webpack_require__(37);
+var XTemplate = __webpack_require__(32);
+var textareaStyle = {
+  width: 600,
+  height: 200
+};
+var labelStyle = {
+  display: 'inline-block',
+  width: 100
+};
 
-});
+function start() {
+  var data = {};
+  var dataContent = document.getElementById('data').value.trim();
+  if (dataContent) {
+    data = JSON.parse(dataContent);
+  }
+  console.log(new XTemplate(document.getElementById('template').value).render(data));
+}
+
+ReactDOM.render(React.createElement(
+  'div',
+  { style: { margin: 10 } },
+  React.createElement(
+    'div',
+    { style: { marginBottom: 10 } },
+    React.createElement(
+      'span',
+      { style: labelStyle },
+      'template:'
+    ),
+    React.createElement('textarea', {
+      style: textareaStyle,
+      id: 'template',
+      defaultValue: 'Hello {{world}}!'
+    })
+  ),
+  React.createElement(
+    'div',
+    { style: { marginBottom: 10 } },
+    React.createElement(
+      'span',
+      { style: labelStyle },
+      'data(json):'
+    ),
+    React.createElement('textarea', {
+      style: textareaStyle,
+      id: 'data',
+      defaultValue: '{ "world": "world"}'
+    })
+  ),
+  React.createElement(
+    'button',
+    { onClick: start },
+    'render to console'
+  )
+), document.getElementById('__react-content'));
+
+/***/ })
+
+},[344]);
 //# sourceMappingURL=playground.js.map
